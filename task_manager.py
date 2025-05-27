88faf702f4bd7abbb052e52d4d6d0fe0c1f3ae8d
Thayna Costa <thaynab170@gmail.com>
2025-05-27 15:01:00 -0400

    feat: adicionar função de remoção de tarefas da lista

--- task_manager.py
+++ task_manager.py
@@ -1,10 +1,39 @@
-def adicionar():
-    pass
+from storage import salvar_tarefas, carregar_tarefas
+import json
+
+def adicionar(descricao: str):
+    """Adiciona uma nova tarefa à lista existente e salva no arquivo 'tasks.json'
+
+    Args:
+        descricao (str): descrição da tarefa a ser adicionada
+    """
+    tasks = carregar_tarefas()
+
+    ids = [t['id'] for t in tasks]
+    proximo_id = max(ids) + 1 if ids else 1
+
+    nova_tarefa = {
+        "id": proximo_id,
+        "descricao": descricao,
+        "status": "pendente"
+    }
+    tasks.append(nova_tarefa)
+    salvar_tarefas(tasks)
 
 
-def remover():
-    pass
+def remover(indice_tarefa: int):
+    """Remove uma tarefa da lista conforme parâmetro passado
 
+    Args:
+        indice_tarefa (int): índice da tarefa a ser excluída
+    """
+    tasks = carregar_tarefas()
+    for task in tasks:
+        if task["id"] == indice_tarefa:
+            tasks.remove(task)
+            break
+    salvar_tarefas(tasks)
+        
 
 def listar():
     pass
