fe3629cead9bfda220121d0091b04a4b1286de07
Thayna Costa <thaynab170@gmail.com>
2025-05-30 19:05:33 -0400

    feat: adicionar prints de msgs de sucesso, não encontrado ou erro

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def formatar_tarefa(id, status, descricao):
     print(f"{id} {status} {descricao}")
 
 def exibir_quantidades(pendentes, concluidas):
-    print("== E S T A T Í S T I C A S ==")
+    print_menu("ESTATÍSTICAS")
     print(f"Número de tarefas pendentes ❌: {pendentes}")
     print(f"Número de tarefas concluídas ✅: {concluidas}")
 
@@ -22,3 +22,35 @@ def input_inteiro(escolha: str) -> int:
         return valor
     except ValueError:
         print("Opção inválida. Digite um número inteiro.")
+
+def print_menu(texto):
+    width = 110
+    border = "="*width
+    empty_line = f"¦{' ' * (width - 2)}¦"
+    print(border)
+    print(empty_line)
+    print(empty_line)
+    print(f"¦{texto.center(width-2)}¦")
+    print(empty_line)
+    print(empty_line)
+    print(border)
+
+def msg_info(texto):
+    print("\n" + "="*50)
+    print(f"{texto}")
+    print("="*50 + "\n")
+
+def msg_alerta(texto):
+    print("\n" + "="*50)
+    print(f"{texto}")
+    print("="*50 + "\n")
+
+def msg_erro(label):
+    print(f"Erro inesperado ao tentar remover tarefa: {label}")
+
+def sem_tarefa():
+    print("Não há tarefas armazenadas!")
+
+def nao_encontrada():
+     print("Não foi possível remover tarefa! Indíce inexistente.")
+
